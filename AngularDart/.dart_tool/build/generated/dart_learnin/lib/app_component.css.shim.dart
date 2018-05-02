b66f111ae54d6e24ce8bae48bb9fc64a38a611dc
Scott Nesham <sanesham@gmail.com>
2018-05-02 00:12:26 -0500

    remove generated files from git

--- AngularDart/.dart_tool/build/generated/dart_learnin/lib/app_component.css.shim.dart
+++
@@ -1,4 +0,0 @@
-library asset_dart_learnin_lib_app_component.css.shim.dart;
-
-
-const List<dynamic> styles = const ['._nghost-%COMP% { } .first._ngcontent-%COMP% { color:#2196F3; } .is-saved._ngcontent-%COMP% { color:#ccc; } .is-saved._ngcontent-%COMP% .first._ngcontent-%COMP% { color:#ddd; }'];
